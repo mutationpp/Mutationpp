45bc8029bfbc8f04b1584d67e3f0b74955345382
Dinesh Ramanathan <trash@trash.mail>
2013-03-13 14:51:54 +0000

    sample test

---
+++ sampleTests/test/CMakeFiles/test_class_Position.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_class_Position.dir/test_class_Position.cpp.o"
+  "test_class_Position.pdb"
+  "test_class_Position"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_class_Position.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ sampleTests/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/didi/mutation++/branches/dinesh/sampleTests/test
+# Build directory: /home/didi/mutation++/branches/dinesh/sampleTests/test
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(test_main_name_displayed "/home/didi/mutation++/branches/dinesh/sampleTests/test/test_class_Position")
