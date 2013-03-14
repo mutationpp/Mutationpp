a0716840d39a25520df8d9d27bc9e429ba05afd4
Dinesh Ramanathan <trash@trash.mail>
2013-03-14 12:27:46 +0000

    Test 2

---
+++ myTets2/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/didi/mutation++/branches/dinesh/sampleTests
+# Build directory: /home/didi/mutation++/branches/dinesh/sampleTests
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(test)
