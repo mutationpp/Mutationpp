d74b94f4e9595165eec2318372599d1ec901928b
Dinesh Ramanathan <trash@trash.mail>
2013-03-14 12:46:59 +0000

    added files

---
+++ myTets2/CTestConfigNightly.cmake
@@ -0,0 +1,63 @@
+##########################################################################
+# Site-specific setup
+##########################################################################
+
+# Dashboard model (Continuous, Experimental, Nightly)
+set(MODEL Continuous)
+
+# source dir(<> dev directory)
+set(CTEST_SOURCE_DIRECTORY "/home/didi/mutation++_cdash/src")
+
+# build dir
+set(CTEST_BINARY_DIRECTORY "/home/didi/mutation++_cdash/build")
+
+# Build and site identification
+set(CTEST_SITE "VM-Ubuntu12.04")
+set(CTEST_BUILD_NAME "gcc4.6.3-Release-Main")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CF_BUILD_TYPE "Release")
+
+# commands
+set(CTEST_CMAKE_COMMAND "/usr/local/bin/cmake")
+set(CTEST_UPDATE_COMMAND "/usr/bin/svn")
+
+
+##########################################################################
+# Configuration
+##########################################################################
+
+# Initial cache entries
+set(CF_CONFIG_OPTIONS " -DCMAKE_BUILD_TYPE:STRING=${CF_BUILD_TYPE}")
+set(CF_CONFIG_OPTIONS "${CF_CONFIG_OPTIONS} -DSITE:STRING=${CTEST_SITE} -DBUILDNAME:STRING=${CTEST_BUILD_NAME}")
+
+# Assemble cmake command line
+set(CTEST_CONFIGURE_COMMAND "${CTEST_CMAKE_COMMAND} ${CF_CONFIG_OPTIONS}")
+set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"-G${CTEST_CMAKE_GENERATOR}\"")
+set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"${CTEST_SOURCE_DIRECTORY}\"")
+
+##########################################################################
+# Run the dashboard
+##########################################################################
+
+#ctest_start (${MODEL})			# demarrage
+#ctest_update(RETURN_VALUE HAD_UPDATES)	# svn update
+#ctest_configure()			# cmake (...)
+#ctest_build()				# make
+#ctest_test()				# ctest
+#ctest_submit()				# envoyer au serveur
+
+
+while (${CTEST_ELAPSED_TIME} LESS 72000)
+ set(START_TIME ${CTEST_ELAPSED_TIME})
+ ctest_start (${MODEL})
+ ctest_update(RETURN_VALUE HAD_UPDATES)
+ if(${HAD_UPDATES} GREATER 0)
+  ctest_configure()
+  ctest_build()
+  ctest_test()
+  ctest_submit()
+ endif()
+ ctest_sleep( ${START_TIME} 60 ${CTEST_ELAPSED_TIME})
+endwhile()
+
+
---
+++ myTets2/test1.cpp
@@ -0,0 +1,34 @@
+/*
+ * test_class_De.cpp
+ *
+ *  Created on: Mar 7, 2013
+ *      Author: didi
+ */
+
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Test Module for Boost Testings"
+#include <iostream>
+#include <fstream>
+#include <string>
+//#include <boost/test/unit_test.hpp>
+//#include <boost/filesystem.hpp>
+
+using namespace std;
+
+
+
+int main() {
+	int pos = 0 ;
+	int somme;
+
+	cout << " pos == 12 " << (pos == 12) << endl;
+	cout << " ( pos + 12 ) == 12 " << (( pos + 12 ) == 12) << endl;
+//	BOOST_CHECK(pos == 0);
+//	BOOST_CHECK_EQUAL((pos+2), 14);
+
+}
+
+
+
+
