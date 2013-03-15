d35cfeabebe4135cc178415f0cfd6d8be7fcc313
Dinesh Ramanathan <trash@trash.mail>
2013-03-15 15:28:28 +0000

    ADD ALL FILES

---
+++ test_mutation/test/test/readFile.hpp
@@ -0,0 +1,22 @@
+/*
+ * readFile.hpp
+ *
+ *  Created on: Mar 14, 2013
+ *      Author: didi
+ */
+
+#ifndef READFILE_HPP_
+#define READFILE_HPP_
+
+
+class ReadFile {
+public :
+	bool compareFiles();
+
+
+
+};
+
+
+
+#endif /* READFILE_HPP_ */
