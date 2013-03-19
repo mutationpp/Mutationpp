511c7a929731c90b8a7bafba1ff4b8d5e0a833d5
Dinesh Ramanathan <trash@trash.mail>
2013-03-19 13:51:01 +0000

    modifs

--- mutation_tests/test_src/test_thermo.cpp
+++ mutation_tests/test_src/test_thermo.cpp
@@ -18,6 +18,17 @@ MPP_TEST_FIXTURE( Air11RRHO, "Air11-RRHO", "air11",
 
 BOOST_FIXTURE_TEST_SUITE(Thermodynamics, Air11RRHO)
 
+
+BOOST_AUTO_TEST_CASE(MixtureCp)
+{
+    string path = boost::filesystem::current_path().generic_string();
+
+cout << endl << endl<< endl  "Path ==> " << path << endl << endl << endl; 
+    compareEquilibriumValues("/home/didi/mutation++/branches/dinesh/dinesh/mutation_tests/test_src/air11/RRHO/cp.dat", cp, 2);
+}
+
+
+
 BOOST_AUTO_TEST_CASE(SetState)
 {
     double T = 1000.0;
@@ -31,14 +42,6 @@ BOOST_AUTO_TEST_CASE(SetState)
     cout << "mix()->nSpecies() : " << mix()->nSpecies() << endl;
     for (int i = 0; i < mix()->nSpecies(); ++i)
         BOOST_CHECK_EQUAL(mix()->X()[i] , 1.0 / mix()->nSpecies());
-}
-
-BOOST_AUTO_TEST_CASE(MixtureCp)
-{
-    string path = boost::filesystem::current_path().generic_string();
-
-cout << endl << endl<< endl  "Path ==> " << path << endl << endl << endl; 
-    compareEquilibriumValues("/home/didi/mutation++/branches/dinesh/dinesh/mutation_tests/test_src/air11/RRHO/cp.dat", cp, 2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
