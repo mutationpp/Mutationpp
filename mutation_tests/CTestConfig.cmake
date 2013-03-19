cca15f62ce62f84602159a75371c20017231babe
Dinesh Ramanathan <trash@trash.mail>
2013-03-19 15:35:58 +0000

    modifs

--- mutation_tests/CTestConfig.cmake
+++ mutation_tests/CTestConfig.cmake
@@ -2,11 +2,11 @@
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
 
-set(CTEST_PROJECT_NAME "mutationtestMixtures") 
+set(CTEST_PROJECT_NAME "Mixtures") 
 set(CTEST_NIGHTLY_START_TIME "23:00:00 GMT")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "localhost")
-set(CTEST_DROP_LOCATION "/submit.php?project=mutationtest&subproject=Mixtures")
+set(CTEST_DROP_LOCATION "/submit.php?project=Mixtures")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(CTEST_UPDATE_TYPE "svn")
