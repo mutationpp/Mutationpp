4a835969c04f600f0421508781dd0b356044c0e9
Dinesh Ramanathan <trash@trash.mail>
2013-03-19 15:33:46 +0000

    modifs

--- mutation_tests/CTestConfig.cmake
+++ mutation_tests/CTestConfig.cmake
@@ -6,7 +6,7 @@ set(CTEST_PROJECT_NAME "mutationtestMixtures")
 set(CTEST_NIGHTLY_START_TIME "23:00:00 GMT")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "localhost")
-set(CTEST_DROP_LOCATION "/submit.php?project=mutationtest")
+set(CTEST_DROP_LOCATION "/submit.php?project=Mixtures")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(CTEST_UPDATE_TYPE "svn")
