7f94b55da22365403605a33fd25f09862fa22b8a
Dinesh Ramanathan <trash@trash.mail>
2013-03-22 13:15:56 +0000

    test in the server

--- mutation_tests/test_src/MppTestFixture.h
+++ mutation_tests/test_src/MppTestFixture.h
@@ -1,5 +1,5 @@
 
-#include "/home/didi/mutation++/trunk/install/include/mutation++/mutation++.h"
+#include "/home/dinesh/mutation++/trunk/install/include/mutation++/mutation++.h"
 #include <iostream>
 #include <string>
 #include <fstream>
