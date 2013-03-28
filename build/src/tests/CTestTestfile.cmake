46bce26d306e23a7c2cecb76f5996843a30960c2
Dinesh Ramanathan <trash@trash.mail>
2013-03-28 14:01:12 +0000

    removed build

--- build/src/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/didi/mutation++/branches/dinesh/src/tests
-# Build directory: /home/didi/mutation++/branches/dinesh/build/src/tests
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test_thermo_class "/home/didi/mutation++/branches/dinesh/build/src/tests/test_thermo_class")
