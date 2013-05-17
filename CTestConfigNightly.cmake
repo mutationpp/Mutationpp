b9d3bf84b90660d9588890124ca5dad11c8d8af2
Dinesh Ramanathan <trash@trash.mail>
2013-05-17 09:40:03 +0000

    changes ctest's log level for more detailes

--- CTestConfigNightly.cmake
+++ CTestConfigNightly.cmake
@@ -56,22 +56,22 @@ set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"${CTEST_SOURCE_DIRECTO
 # Run the dashboard
 ##########################################################################
 
-#ctest_start (${MODEL})			# demarrage
-#ctest_update(RETURN_VALUE HAD_UPDATES)	# svn update
-#ctest_configure()			# cmake (...)
-#ctest_build()				# make
-#ctest_test()				# ctest = make test
-#ctest_submit()				# envoyer au serveur
+ctest_start (Nightly)			# demarrage
+ctest_update(RETURN_VALUE HAD_UPDATES)	# svn update
+ctest_configure()			# cmake (...)
+ctest_build()				# make
+ctest_test()				# ctest = make test
+ctest_submit()				# envoyer au serveur
 
 
- set(START_TIME ${CTEST_ELAPSED_TIME})
-ctest_start (${MODEL})
- ctest_update(RETURN_VALUE HAD_UPDATES)
+
+#ctest_start (${MODEL})
+# ctest_update(RETURN_VALUE HAD_UPDATES)
 # if(${HAD_UPDATES} GREATER 0)
-  ctest_configure()
-  ctest_build()		
-  ctest_test()
-  ctest_submit()
+#  ctest_configure()
+#  ctest_build()		
+#  ctest_test()
+ # ctest_submit()
   execute_process(COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/mailto ${MY_TEST_PATH})#${MY_MAIL_TO} )
 # else(${HAD_UPDATES} GREATER 0)
 #	MESSAGE("There are no new updates since last regression test")	
@@ -80,13 +80,6 @@ ctest_start (${MODEL})
 #execute_process(COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/mailto ${MY_TEST_PATH})#${MY_MAIL_TO} )
 
 
-
-
- if(${HAD_UPDATES} GREATER 0)
- 	MESSAGE("There are some new updates since last regression test ==> ${HAD_UPDATES} ") 
- else()
-       MESSAGE("There are no new updates since last regression test ==> ${HAD_UPDATES}")  
- endif()
 
 
 
