4539c3e072fe4996076c64b871d82221a14fe0e6
JB Scoggins <scoggins@vki.ac.be>
2014-09-11 16:07:02 +0000

    - Moved getTFromRhoE to the StateModel class so that it is available to all classes which inherit from StateModel

--- src/thermo/ChemNonEqStateModel.cpp
+++ src/thermo/ChemNonEqStateModel.cpp
@@ -22,25 +22,6 @@ public:
         delete [] mp_work;
     }
 
-    // Define the H and Cp providers
-    class Cp {
-    public:
-        const Thermodynamics& thermo;
-        Cp(const Thermodynamics& t) : thermo(t) {}
-        void operator () (double T, double* const cp) const {
-            thermo.speciesCpOverR(T, cp);
-        }
-    };
-
-    class H {
-    public:
-        const Thermodynamics& thermo;
-        H(const Thermodynamics& t) : thermo(t) {}
-        void operator () (double T, double* const cp) const {
-            thermo.speciesHOverRT(T, cp);
-        }
-    };
-
     /**
      * Sets the current state of the mixture.  Variable sets can be the
      * following:
@@ -67,7 +48,8 @@ public:
         switch (vars) {
         case 0:
             // Solve energy equation
-            getTFromRhoE(Cp(m_thermo), H(m_thermo), p_energy[0], m_T, -conc);
+            getTFromRhoE(
+                Cp(m_thermo), H(m_thermo), p_energy[0], m_T, mp_work, -conc);
             break;
         case 1:
             // Check that temperature is at least positive
@@ -88,93 +70,36 @@ public:
         m_P = RU * m_T * conc;
     }
 
-protected:
+private:
 
     /**
-     * Solves the general form of an energy equation
-     * \f[ f(T) = T \left[\sum_i \tilde{\rho}_i \left(\frac{H_i(T)}{R_uT}\right)
-     *            + \alpha\right] - \frac{\rho e}{R_u} = 0 \f]
-     * for the temperature given the \f$C_{p,i}/R_u\f$ and \f$H_i/R_uT\f$
-     * function providers using a Newton-Rhapson iterative procedure.  This
-     * function assumes that the mp_X array is filled with the species molar
-     * densities (concentrations) prior to being called. The convergence
-     * criteria for the Newton iterations is taken to be
-     * \f[ |f(T)| < \epsilon_r\left|\frac{\rho e}{R_u}\right| + \epsilon_a \f]
-     * where \f$\epsilon_a\f$ and \f$\epsilon_r\f$ are absolute and relative
-     * tolerances respectively.  Note that if the total energy equation is
-     * being solved, \f$\alpha\f$ should be set to the negative mixture
-     * concentration, \f$-\sum \tilde{\rho}_i\f$. For internal energy equations,
-     * \f$\alpha = 0\f$.
-     *
-     * @param cp        class providing species specific heats
-     * @param h         class providing species enthalpies
-     * @param rhoe      value of \f$\rho e\f$
-     * @param T         initial guess on input, output is the solution
-     * @param alpha     arbitrary temperature coefficient in energy equation
-     * @param atol      absolute tolerance, \f$\epsilon_a\f$, on \f$f\f$
-     * @param rtol      relative tolerance, \f$\epsilon_r\f$, on \f$f\f$
-     * @param max_iters maximum number of iterations (warns user if exceeded)
-     *
-     * @return false if max iterations exceeded, true otherwise
+     * Small helper class which provides wrapper to
+     * Thermodynamics::speciesCpOverR() to be used with getTFromRhoE().
      */
-    template <typename CpProvider, typename HProvider>
-    bool getTFromRhoE(
-        const CpProvider& cp,
-        const HProvider& h,
-        const double rhoe,
-        double& T,
-        const double alpha = 0.0,
-        const double atol = 1.0e-12,
-        const double rtol = 1.0e-12,
-        const int max_iters = 10)
-    {
-        const int ns = m_thermo.nSpecies();
-        const double rhoe_over_Ru = rhoe/RU;
-        const double tol = rtol*std::abs(rhoe_over_Ru) + atol;
-
-        double f, fp, dT;
-
-        // Compute initial value of f
-        h(T, mp_work);
-        f = alpha;
-        for (int i = 0; i < ns; ++i)
-            f += mp_X[i]*mp_work[i];
-        f = T*f - rhoe_over_Ru;
-
-        int iter = 0;
-        //cout << iter << " " << f << " " << T << endl;
-        while (std::abs(f) > tol) {
-            // Check for max iterations
-            if (iter++ == max_iters) {
-                using std::cerr;
-                cerr << "Exceeded max iterations when computing temperature!\n";
-                cerr << "res = " << f / rhoe_over_Ru << ", T = " << T << endl;
-                return false;
-            }
-
-            // Compute df/dT
-            cp(T, mp_work);
-            fp = alpha;
-            for (int i = 0; i < ns; ++i)
-                fp += mp_X[i]*mp_work[i];
-
-            // Update T
-            dT = f/fp;
-            while (dT >= T) dT *= 0.5; // prevent non-positive T
-            T -= dT;
-
-            // Recompute f
-            h(T, mp_work);
-            f = alpha;
-            for (int i = 0; i < ns; ++i)
-                f += mp_X[i]*mp_work[i];
-            f = T*f - rhoe_over_Ru;
-            //cout << iter << " " << f << " " << T << endl;
+    class Cp {
+    public:
+        Cp(const Thermodynamics& t) : thermo(t) {}
+        void operator () (double T, double* const cp) const {
+            thermo.speciesCpOverR(T, cp);
         }
+    private:
+        const Thermodynamics& thermo;
+    };
 
-        // Let the user know if we converged or not
-        return true;
-    }
+
+    /**
+     * Small helper class which provides wrapper to
+     * Thermodynamics::speciesHOverRT() to be used with getTFromRhoE().
+     */
+    class H {
+    public:
+        H(const Thermodynamics& t) : thermo(t) {}
+        void operator () (double T, double* const h) const {
+            thermo.speciesHOverRT(T, h);
+        }
+    private:
+        const Thermodynamics& thermo;
+    };
 
 private:
 
--- src/thermo/StateModel.h
+++ src/thermo/StateModel.h
@@ -11,29 +11,17 @@ namespace Mutation {
 
 
 /**
- * Simple interface for any object who wants to be updated whenever the a
- * StateModel changes its state.
- */
-//class StateModelWatcher
-//{
-//protected:
-//    void stateChanged() = 0;
-//}
-
-
-/**
  * Base class for all state models.  A mixture state is completely determined
  * when enough thermodynamic values are combined with the mixture composition
  * such that all other mixture quantities can be successfully determined.  For 
- * example, with a simple mixture modelled in thermal equilibrium, the mixture
+ * example, with a simple mixture modeled in thermal equilibrium, the mixture
  * temperature, pressure, and species mole fractions are enough to fully
  * determine all other mixture quantities.
  *
- * A state model describes how a particular mixture is to be modelled.  For
- * example, a mixture could be modelled using a single temperature or multiple
+ * A state model describes how a particular mixture is to be modeled.  For
+ * example, a mixture could be modeled using a single temperature or multiple
  * temperatures that represent the various energy modes in the mixture.  This
- * base class provides the framework for defining new state models above the 
- * simple T,P model.
+ * base class provides the framework for defining new state models.
  */
 class StateModel
 {
@@ -63,8 +51,6 @@ public:
     {
         delete [] mp_X;
     }
-    
-//    void addWatcher(StateModelWatcher* p_watcher);
     
     /**
      * Returns the number of energy equations represented by this StateModel.
@@ -168,10 +154,96 @@ public:
         return NULL;
     }
     
-//protected:
+protected:
 
-//    void notifyWatchers();
-    
+    /**
+     * Solves the general form of an energy equation
+     * \f[ f(T) = T \left[\sum_i \tilde{\rho}_i \left(\frac{H_i(T)}{R_uT}\right)
+     *            + \alpha\right] - \frac{\rho e}{R_u} = 0 \f]
+     * for the temperature given the \f$C_{p,i}/R_u\f$ and \f$H_i/R_uT\f$
+     * function providers using a Newton-Rhapson iterative procedure.  This
+     * function assumes that the mp_X array is filled with the species molar
+     * densities (concentrations) prior to being called. The convergence
+     * criteria for the Newton iterations is taken to be
+     * \f[ |f(T)| < \epsilon_r\left|\frac{\rho e}{R_u}\right| + \epsilon_a \f]
+     * where \f$\epsilon_a\f$ and \f$\epsilon_r\f$ are absolute and relative
+     * tolerances respectively.  Note that if the total energy equation is
+     * being solved, \f$\alpha\f$ should be set to the negative mixture
+     * concentration, \f$-\sum \tilde{\rho}_i\f$. For internal energy equations,
+     * \f$\alpha = 0\f$.
+     *
+     * @param cp        class providing species specific heats
+     * @param h         class providing species enthalpies
+     * @param rhoe      value of \f$\rho e\f$
+     * @param T         initial guess on input, output is the solution
+     * @param p_work    work array, at least number of species long
+     * @param alpha     arbitrary temperature coefficient in energy equation
+     * @param atol      absolute tolerance, \f$\epsilon_a\f$, on \f$f\f$
+     * @param rtol      relative tolerance, \f$\epsilon_r\f$, on \f$f\f$
+     * @param max_iters maximum number of iterations (warns user if exceeded)
+     *
+     * @return false if max iterations exceeded, true otherwise
+     */
+    template <typename CpProvider, typename HProvider>
+    bool getTFromRhoE(
+        const CpProvider& cp,
+        const HProvider& h,
+        const double rhoe,
+        double& T,
+        double* const p_work,
+        const double alpha = 0.0,
+        const double atol = 1.0e-12,
+        const double rtol = 1.0e-12,
+        const int max_iters = 10)
+    {
+        const int ns = m_thermo.nSpecies();
+        const double rhoe_over_Ru = rhoe/RU;
+        const double tol = rtol*std::abs(rhoe_over_Ru) + atol;
+
+        double f, fp, dT;
+
+        // Compute initial value of f
+        h(T, p_work);
+        f = alpha;
+        for (int i = 0; i < ns; ++i)
+            f += mp_X[i]*p_work[i];
+        f = T*f - rhoe_over_Ru;
+
+        int iter = 0;
+        //cout << iter << " " << f << " " << T << endl;
+        while (std::abs(f) > tol) {
+            // Check for max iterations
+            if (iter++ == max_iters) {
+                using std::cerr;
+                cerr << "Exceeded max iterations when computing temperature!\n";
+                cerr << "res = " << f / rhoe_over_Ru << ", T = " << T << endl;
+                return false;
+            }
+
+            // Compute df/dT
+            cp(T, p_work);
+            fp = alpha;
+            for (int i = 0; i < ns; ++i)
+                fp += mp_X[i]*p_work[i];
+
+            // Update T
+            dT = f/fp;
+            while (dT >= T) dT *= 0.5; // prevent non-positive T
+            T -= dT;
+
+            // Recompute f
+            h(T, p_work);
+            f = alpha;
+            for (int i = 0; i < ns; ++i)
+                f += mp_X[i]*p_work[i];
+            f = T*f - rhoe_over_Ru;
+            //cout << iter << " " << f << " " << T << endl;
+        }
+
+        // Let the user know if we converged or not
+        return true;
+    }
+
 protected:
 
     const Thermodynamics& m_thermo;
@@ -187,12 +259,7 @@ protected:
     
     double* mp_X;
     
-//private:
-    
-//    std::list<StateModelWatcher*> m_watcher_list;
-    
 }; // class StateModel
-
 
     } // namespace Thermodynamics
 } // namespace Mutation
