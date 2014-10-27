eac430a39036ad7a74d53cd25997af33c3caf49c
JB Scoggins <scoggins@vki.ac.be>
2014-10-27 17:27:38 +0000

    - Added license header to each file in tests directory

--- tests/test_wdot.cpp
+++ tests/test_wdot.cpp
@@ -1,10 +1,31 @@
 /**
  * @file test_wdot.cpp
  *
- * Contains tests related to the species production rates in Mutation++.  A
- * mixture name must be given in the command line arguments for the tests to
+ * @brief Contains tests related to the species production rates in Mutation++.
+ * A mixture name must be given in the command line arguments for the tests to
  * work and all tests are performed on this mixture and its associated
  * mechanism.
+ */
+
+/*
+ * Copyright 2014 von Karman Institute for Fluid Dynamics (VKI)
+ *
+ * This file is part of MUlticomponent Thermodynamic And Transport
+ * properties for IONized gases in C++ (Mutation++) software package.
+ *
+ * Mutation++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Mutation++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Mutation++.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #define BOOST_TEST_DYN_LINK
