85199948733a2ce7cdc76861542dea992db53ce3
JB Scoggins <scoggins@vki.ac.be>
2016-01-22 07:33:53 +0000

    - small work on SMB function

--- src/general/Mixture.cpp
+++ src/general/Mixture.cpp
@@ -120,7 +120,8 @@ double Mixture::smb(
     const double eps = 1.0e-8;
     const double tol = 1.0e-6;
     const double alpha = 0.05;
-    const int maxits = 10;
+    const int maxits = 20;
+    const int ns = nSpecies();
 
     Map<VectorXd> rhoi(p_rhoi, nSpecies());
     VectorXd rhoit(nSpecies());
@@ -134,16 +135,16 @@ double Mixture::smb(
     VectorXd dp = (xip - Map<const VectorXd>(X(), nSpecies())) / dx;
 
     // Compute f
-    VectorXd f(nSpecies()); smbf(Tw, Pw, dp, f);
-    VectorXd fp(nSpecies());
-    MatrixXd jac(nSpecies(), nSpecies());
-    VectorXd d(nSpecies());
+    VectorXd f(ns); smbf(Tw, Pw, dp, f);
+    VectorXd fp(ns);
+    MatrixXd jac(ns, ns);
+    VectorXd d(ns);
     cout << "norm(f) = " << f.norm() << endl;
     cout << "f = \n" << f << endl;
 
     for (int newt = 0; newt < maxits && f.norm() > tol; ++newt) {
         // Compute the Jacobian
-        for (int j = 0; j < nSpecies(); ++j) {
+        for (int j = 0; j < ns; ++j) {
             // Perturb mass fraction of species j
             double h = std::max(eps*rhoi[j], 1.0e-20);
             double rhoj = rhoi[j];
@@ -153,15 +154,19 @@ double Mixture::smb(
             setState(rhoi.data(), Tvec.data(), 1);
 
             // Compute f
-            dp = (xip - Map<const VectorXd>(X(), nSpecies())) / dx;
+            dp = (xip - Map<const VectorXd>(X(), ns)) / dx;
             smbf(Tw, Pw, dp, fp);
 
             // Column j of jacobian
             jac.col(j) = (fp - f) / h;
+            jac(ns-1,j) = 1.0/speciesMw(j);
 
             // Reset yj
             rhoi[j] = rhoj;
         }
+
+        cout << "jac = \n" << jac << endl;
+        cout << "rank = " << jac.jacobiSvd().rank() << endl;
 
         // Compute update direction
         d = jac.colPivHouseholderQr().solve(f);
@@ -189,6 +194,7 @@ double Mixture::smb(
         cout << "norm(f) = " << f.norm() << endl;
     }
 
+    return 0.0;
 }
 
 void Mixture::smbf(double Tw, double Pw, const VectorXd& dx, VectorXd& f)
@@ -210,10 +216,7 @@ void Mixture::smbf(double Tw, double Pw, const VectorXd& dx, VectorXd& f)
     //    cout << setw(20) << speciesName(i) << " " << mdotci[i] << endl;
 
     // Compute the mass fractions of pyrolysis gas
-    ArrayXd xeg(nElements()); xeg.setZero();
-    xeg[elementIndex("C")] = 0.229;
-    xeg[elementIndex("H")] = 0.661;
-    xeg[elementIndex("O")] = 0.110;
+    ArrayXd xeg(nElements()); getComposition("pyro", xeg.data());
     ArrayXd yig(nSpecies());
     equilibriumComposition(Tw, Pw, xeg.data(), yig.data());
     convert<X_TO_Y>(yig.data(), yig.data());
@@ -246,6 +249,7 @@ void Mixture::computeSurfaceSource(ArrayXd& mci)
                 if (j == ns)
                     cout << "Warning: ion without parent neutral in smb." << endl;
                 else {
+                    //cout << "ion = " << speciesName(i) << ", neutral = " << speciesName(j) << endl;
                     // Add mass flux contributions
                     double mwi = speciesMw(i);
                     double flux = std::sqrt(RU*T()/(TWOPI*mwi))*rho*Y()[i];
@@ -259,8 +263,6 @@ void Mixture::computeSurfaceSource(ArrayXd& mci)
         mci[0] = -std::sqrt(RU*T()/TWOPI*speciesMw(0))*rho*Y()[0];
     }
 
-
-
     // Surface reactions
     int iO  = speciesIndex("O");
     int iO2 = speciesIndex("O2");
@@ -272,22 +274,22 @@ void Mixture::computeSurfaceSource(ArrayXd& mci)
     double mdot, fac = std::sqrt(RU*T()/TWOPI)*rho;
     // O + C(s) -> CO
     mdot = fac/std::sqrt(speciesMw(iO))*Y()[iO]*0.63*std::exp(-1160.0/T());
-    mci[iO]  -= mdot;
-    mci[iCO] += mdot*speciesMw(iCO)/speciesMw(iO);
+    mci[iO]  += mdot;
+    mci[iCO] -= mdot*speciesMw(iCO)/speciesMw(iO);
 
     // O2 + 2C(s) -> 2CO
     mdot = fac/std::sqrt(speciesMw(iO2))*Y()[iO2];
-    mci[iO2] -= mdot;
-    mci[iCO] += 2.0*speciesMw(iCO)/speciesMw(iO2);
+    mci[iO2] += mdot;
+    mci[iCO] -= 2.0*speciesMw(iCO)/speciesMw(iO2);
 
     // N + C(s) -> CN
     mdot = fac/std::sqrt(speciesMw(iN))*Y()[iN]*3.0e-3;
-    mci[iN]  -= mdot;
-    mci[iCN] += mdot*speciesMw(iCN)/speciesMw(iN);
+    mci[iN]  += mdot;
+    mci[iCN] -= mdot*speciesMw(iCN)/speciesMw(iN);
 
     // 3C(s) -> C3
     double yeq = 5.19e14*std::exp(-90845.0/T())*speciesMw(iC3)/(RU*T()*rho);
-    mci[iC3] += fac/std::sqrt(speciesMw(iC3))*(yeq - Y()[iC3]);
+    mci[iC3] -= fac/std::sqrt(speciesMw(iC3))*(yeq - Y()[iC3]);
 
     //mci.fill(0.0);
 }
