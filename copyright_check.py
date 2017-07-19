98e4f2f40bd373bc5e5180f66cca20920d53b1a7
Vincent Leroy <vincent.leroy@vki.ac.be>
2017-07-19 19:59:08 +0200

    First fully functioning version
    - Copying statement check is now done using regex and should work with any type of source file
    - Copyright info check now works as intended
    - The script now allows to update the date of files with correctly formatted copyright info, with possible backup
    - Still not tested on non-C/C++ source files

--- copyright_check.py
+++ copyright_check.py
@@ -1,7 +1,12 @@
+# TODO: single-file check
+# TODO: test of Python and Fortran sources
+
 import os
 import argparse
 import fnmatch
 import re
+import datetime
+import shutil
 
 
 class colors:
@@ -48,122 +53,226 @@ class colors:
         lightgrey = '\033[47m'
 
 
-# Constants defining defaults for the copyright information note
-YEAR = "2014"
+# Constants defining defaults for the copyright information and the copying statement
+YEAR = datetime.date.today().year
 NAME = "von Karman Institute for Fluid Dynamics (VKI)"
+STATEMENT_TEXT = """This file is part of MUlticomponent Thermodynamic And Transport
+properties for IONized gases in C++ (Mutation++) software package.
+
+Mutation++ is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Mutation++ is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with Mutation++.  If not, see
+<http://www.gnu.org/licenses/>."""
 
 # Constants defining strings used for verbose reports
-CPINFO_PRESENT_STRING = \
-    {True: colors.fg.green + "present" + colors.reset,
-     False: colors.fg.red + "missing" + colors.reset}
 CPINFO_CORRECTLY_FORMATTED_STRING = \
     {True: colors.fg.green + "correctly formatted" + colors.reset,
-     False: colors.fg.red + "improperly formatted" + colors.reset}
-CPINFO_UPTODATE_STRING = \
-    {True: colors.fg.green + "up-to-date" + colors.reset,
-     False: colors.fg.red + "outdated" + colors.reset}
-STATEMENT_GOOD_STRING = \
+     False: colors.fg.red + "incorrect or missing" + colors.reset}
+CPINFO_DATE_CORRECT_STRING = \
+    {True: colors.fg.green + "correct" + colors.reset,
+     False: colors.fg.red + "incorrect" + colors.reset}
+STATEMENT_CORRECT_STRING = \
     {True: colors.fg.green + "correct" + colors.reset,
      False: colors.fg.red + "incorrect" + colors.reset}
 
 
 class FileInfo(object):
-    def __init__(self, filename):
+    def __init__(self, filename, statement_text):
+        """Default constructor.
+
+        :param filename: Path to the file being checked.
+        :param statement_text: Text for the copying statement."""
+
         self.filename = filename
-        self.statement_good = False
-        self.cpinfo_present = False
-        self.cpinfo_correctly_formatted = False
-        self.cpinfo_uptodate = False
-        self.cpinfo_line = ''
+        self.statement_text = statement_text
+        self.statement_correct = None
+        self.cpinfo_correctly_formatted = None
+        self.cpinfo_date_correct = None
+        self.cpinfo_line = None
+        self.cpinfo_line_corrected = None
 
-    def check_copyright(self, year=YEAR, name=NAME):
-        # Check copyright info
-        with open(self.filename, 'r') as source_file:
-            for source_line in source_file:
-                # Check presence
-                # TODO: this presence check may yield many false positives and should be deleted
-                if re.match("(.*)(C|c)opyright(.*)", source_line):
-                    self.cpinfo_present = True
-                    self.cpinfo_line = source_line
+    def check_cpinfo_correctly_formatted(self, name=NAME, force=False):
+        """Checks if the file contains a copyright information line and if it is properly formatted,
+        i.e. if it has the format `Copyright <year> <name>` or `Copyright <year1>-<year2> <name>`.
+        If the update has already been run, it is not run again (unless forced).
 
+        :param name: Author name to be looked up. Defaults to the NAME global variable.
+        :param force: Force update."""
+
+        if self.cpinfo_correctly_formatted is None or force:
+            # Check copyright info format
+            with open(self.filename, 'r') as source_file:
+                for source_line in source_file:
                     # Check format
-                    self.cpinfo_correctly_formatted = \
-                        bool(re.match("(.*)Copyright [0-9]{4}(-[0-9]{4})* (.*)",
-                                      source_line))
+                    search_obj = re.search("Copyright ([0-9]{4})-?([0-9]{4})? " + re.escape(name),
+                                           source_line)
+                    if search_obj is not None:
+                        self.cpinfo_correctly_formatted = True
+                        self.cpinfo_line = search_obj.group(0)
+                        return
 
-                    # Check date and author
-                    self.cpinfo_uptodate = \
-                        bool(
-                            re.match(
-                                "(.*)Copyright {} {}(.*)".format(re.escape(YEAR),
-                                                                 re.escape(NAME)),
-                                source_line))
+            # If no match was found, it means that no correctly formatted copyright info was found
+            self.cpinfo_correctly_formatted = False
 
-    def check_statement(self):
-        # Check copying statement (line by line because of comment symbols)
-        # Load statement lines
-        statement_line_list = []
-        with open('copying_statement.txt', 'r') as statement_file:
-            statement_line_list = [line.strip() for line in statement_file.readlines()]
+    def check_cpinfo_date_correct(self, year=YEAR, force=False):
+        """If the file contains a correctly formatted copyright information line, checks if the
+        date is up-to-date. If not, propose a correction. This method automatically ensures that
+        the copyright information line format has been checked for and forces a check if necessary.
+        If the update has already been run, it is not run again (unless forced).
 
-        with open(self.filename, 'r') as source_file:
-            i_statement_line = 0
+        :param year: Current year. Defaults to the current year.
+        :param force: Force update. This will not force self.check_cpinfo_correctly_formatted()."""
 
-            for source_line in source_file:
-                source_line = source_line.strip(" * ").strip()
+        # First, make sure that a copyright info line was searched for
+        self.check_cpinfo_correctly_formatted()
 
-                statement_line = statement_line_list[i_statement_line]
-                if statement_line == source_line:
-                    i_statement_line += 1
+        if self.cpinfo_date_correct is None or force:
+            if self.cpinfo_correctly_formatted:
+                # Check date
+                if self.cpinfo_correctly_formatted:
+                    # Search and store year info
+                    search_obj = re.search("(?P<first>[0-9]{4})-?(?P<second>[0-9]{4})?",
+                                           self.cpinfo_line)
 
-                if i_statement_line >= len(statement_line_list):
-                    self.statement_good = True
-                    break
+                    # Check if year is up-to-date; if not, propose a correction
+                    if search_obj is not None:
+                        first = int(search_obj.groupdict()['first'])
+                        second = int(search_obj.groupdict()['second']) \
+                            if search_obj.groupdict()['second'] is not None \
+                            else None
+
+                        if second is not None:
+                            if second == year:
+                                self.cpinfo_date_correct = True
+                                self.cpinfo_line_corrected = self.cpinfo_line
+                            else:
+                                self.cpinfo_date_correct = False
+                                self.cpinfo_line_corrected = re.sub(str(second), str(year), self.cpinfo_line)
+                        else:
+                            if first == year:
+                                self.cpinfo_date_correct = True
+                                self.cpinfo_line_corrected = self.cpinfo_line
+                            else:
+                                self.cpinfo_date_correct = False
+                                self.cpinfo_line_corrected = re.sub(str(first), "{}-{}".format(first, year), self.cpinfo_line)
+
+    def check_statement_correct(self, force=False):
+        """Check that the copying statement is correctly formatted. This method work with every comment types,
+        but it requires the paging (newline positioning) and the case to be correct. If not, false
+        negatives or positives are possible.
+        If the update has already been run, it is not run again (unless forced).
+
+        :param force: Force update."""
+
+        if self.statement_correct is None or force:
+            # Check copying statement (line by line because of comment symbols)
+            statement_line_list = [re.escape(line.strip()) for line in self.statement_text.split("\n")]
+
+            # Build the regex pattern
+            pattern = r"\s+.*".join(statement_line_list)
+
+            # Match it with file contents
+            with open(self.filename, 'r') as source_file:
+                if re.search(pattern, source_file.read()):
+                    self.statement_correct = True
+                else:
+                    self.statement_correct = False
 
     def check(self):
-        if      self.filename \
-            and self.cpinfo_present \
-            and self.cpinfo_correctly_formatted \
-            and self.cpinfo_uptodate:
+        self.check_cpinfo_correctly_formatted()
+        self.check_cpinfo_date_correct()
+        self.check_statement_correct()
+
+        if self.filename \
+                and self.cpinfo_correctly_formatted \
+                and self.cpinfo_date_correct \
+                and self.statement_correct:
             return True
         else:
             return False
 
-    def print_report(self, verbose=0):
-        """Build a string reporting the status of the file for which
-        info is stored. Lines after the 1st are indented with 4
-        whitespaces."""
+    def print_report(self, verbose=False):
+        """Report if the file has problems.
+
+        :param verbose: Activate verbose mode and print a detailed report. Lines after
+                        the 1st are indented with 4 whitespaces."""
+
         report_string = colors.bold + self.filename + colors.reset
+
+        if self.check():
+            report_string += \
+                ": " + colors.fg.green + "OK" + colors.reset
+        else:
+            report_string += \
+                ": " + colors.fg.red + "NOT OK" + colors.reset
 
         if verbose:
             report_string += \
-                "\n" + "Copying statement {}".format(
-                    STATEMENT_GOOD_STRING[self.statement_good])
+                "\nCopyright info {}".format(
+                    CPINFO_CORRECTLY_FORMATTED_STRING[self.cpinfo_correctly_formatted])
+
+            if self.cpinfo_correctly_formatted:
+                report_string += \
+                    "\nCopyright info date " + \
+                    "{}".format(CPINFO_DATE_CORRECT_STRING[self.cpinfo_date_correct])
+
+                if not self.cpinfo_date_correct:
+                    report_string += \
+                        "\nCurrent copyright info: " + \
+                        colors.fg.red + \
+                        "{}".format(self.cpinfo_line) + \
+                        colors.reset
+                    report_string += \
+                        "\nCorrected copyright info: " + \
+                        colors.fg.green + \
+                        "{}".format(self.cpinfo_line_corrected) + \
+                        colors.reset
 
             report_string += \
-                "\n" + "Copyright info {}".format(
-                    CPINFO_PRESENT_STRING[self.cpinfo_present])
-
-            if self.cpinfo_present:
-                report_string += \
-                    ", {}".format(
-                        CPINFO_CORRECTLY_FORMATTED_STRING[self.cpinfo_correctly_formatted])
-
-                if self.cpinfo_correctly_formatted:
-                    report_string += \
-                        ", {}".format(
-                            CPINFO_UPTODATE_STRING[self.cpinfo_uptodate])
-
-                report_string += \
-                    "\n" + "{}".format(
-                        self.cpinfo_line.strip(" * ").strip())
+                "\nCopying statement " + \
+                "{}".format(STATEMENT_CORRECT_STRING[self.statement_correct])
 
         print report_string.replace("\n", "\n    ")
+
+    def update_cpinfo_date(self, backup=False):
+        """Update copyright info line if the date is incorrect.
+
+        :param backup: Create backup of the file if modified. If a backup already exists, it is overwritten."""
+
+        self.check_cpinfo_date_correct()
+
+        if self.cpinfo_correctly_formatted and not self.cpinfo_date_correct:
+            # Search for the copyright info line and replace it with the correct one
+            source_data = None
+            with open(self.filename, 'r') as source_file:
+                source_data = source_file.read()
+
+            source_data = source_data.replace(self.cpinfo_line, self.cpinfo_line_corrected)
+
+            if backup:
+                shutil.copy(self.filename, self.filename + ".bak")
+
+            with open(self.filename, 'w') as source_file:
+                source_file.write(source_data)
 
 
 def main(args):
     path_list = args.path.split()
     ext_list = args.extensions.split()
+
+    statement_text = STATEMENT_TEXT
+    if args.statement is not None:
+        with open(args.statement, 'r') as statement_file:
+            statement_text = statement_file.read()
 
     # List files to be checked
     matches = []
@@ -173,15 +282,19 @@ def main(args):
                 for filename in fnmatch.filter(filenames, '*{}'.format(ext)):
                     matches.append(os.path.join(root, filename))
 
-    # Check copyright information
+    # Check copyright information and correct files if requested
     for filename in matches:
-            source_file_info = FileInfo(filename)
+            source_file_info = FileInfo(filename, statement_text)
 
-            source_file_info.check_copyright()
-            source_file_info.check_statement()
+            source_file_info.check()
 
             if not source_file_info.check():
                 source_file_info.print_report(args.verbose)
+
+            if args.update \
+                    and source_file_info.cpinfo_correctly_formatted \
+                    and not source_file_info.cpinfo_date_correct:
+                source_file_info.update_cpinfo_date(backup=args.backup)
 
 
 if __name__ == "__main__":
@@ -193,8 +306,11 @@ if __name__ == "__main__":
                        appropriately formatted. By default, the program lists the names 
                        of files with (1) missing, improperly formatted or outdated copyright 
                        information or (2) incorrect or missing statement of copying 
-                       permission.""")
+                       permission. A default copying statement text is provided, and it 
+                       can be overridden with the --statement option.""")
 
+    parser.add_argument("-b", "--backup", action="store_true",
+                        help="Create backup if files are updated.")
     parser.add_argument("-e", "--extensions", default=" ".join(default_extensions_list),
                         help="File extension to match. Multiple extensions can be "
                              "specified using quotes. "
@@ -204,6 +320,8 @@ if __name__ == "__main__":
                              "should be inspected. Multiple paths can be "
                              "specified using quotes. "
                              "Defaults to \"{}\".".format(" ".join(default_paths_list)))
+    parser.add_argument("-s", "--statement", default=None,
+                        help="File from which the copying statement text is loaded.")
     parser.add_argument("-u", "--update", action="store_true",
                         help="Update copyright information if it is properly formatted.")
     parser.add_argument("-v", "--verbose", action='store_true',
