06c5001be20e13e1c61e1a5c7406a8f65692a7a2
amorosej <julien.amorosetti@vki.ac.be>
2019-03-13 08:32:14 +0100

    omegaI_fixed

--- src/transfer/OmegaI.cpp
+++ src/transfer/OmegaI.cpp
@@ -50,11 +50,13 @@ public:
 		mp_delta = new double [m_nr];
 		for(int i=0; i<m_nr; ++i) {
 			if ( (mix.reactions()[i].type() == Kinetics::IONIZATION_E)
-			  || (mix.reactions()[i].type() == Kinetics::DISSOCIATION_E))
-				 m_rId.push_back(std::make_pair(i,1));
-			if ( (mix.reactions()[i].type() == Kinetics::ION_RECOMBINATION_E)
-			  || (mix.reactions()[i].type() == Kinetics::RECOMBINATION_E))
-				 m_rId.push_back(std::make_pair(i,-1));
+				|| (mix.reactions()[i].type() == Kinetics::DISSOCIATION_E)
+				|| (mix.reactions()[i].type() == Kinetics::ELECTRONIC_DETACHMENT_E)
+				|| (mix.reactions()[i].type() == Kinetics::EXCITATION_E)
+				|| (mix.reactions()[i].type() == Kinetics::ION_RECOMBINATION_E)
+				|| (mix.reactions()[i].type() == Kinetics::RECOMBINATION_E)
+				|| (mix.reactions()[i].type() == Kinetics::ELECTRONIC_ATTACHMENT_E) )
+				m_rId.push_back(i);
 		}
 	};
 
@@ -94,8 +96,8 @@ public:
 		double src = 0.0;
 		int j;
 		for (int i = 0; i < m_rId.size(); ++i) {
-		    j = m_rId[i].first;
-	        src += m_rId[i].second*mp_delta[j]*mp_rate[j];
+			j = m_rId[i];
+			src += mp_delta[j]*mp_rate[j];
 		}
 
 		return (-src*RU*m_mixture.T());
@@ -105,7 +107,7 @@ public:
 private:
 	int m_ns;
 	int m_nr;
-        std::vector<std::pair<int, int> > m_rId;
+	std::vector<int> m_rId;
 	double* mp_hf;
 	double* mp_h;
 	double* mp_rate;
