e33d7194ad69b114373a89165c83d8f383af32c7
Ruben Di Battista <ruben.di-battista@polytechnique.edu>
2019-04-25 16:16:19 +0200

    Check for ENABLE_COVERAGE

--- cmake/modules/CodeCoverage.cmake
+++ cmake/modules/CodeCoverage.cmake
@@ -1,41 +1,3 @@
-# Coverage flags
-if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
-    message(FATAL_ERROR "Not Implemented! Yet. Use GCC for coverage")
-elseif(CMAKE_COMPILER_IS_GNUCXX)
-
-    find_program(GCOV_EXECUTABLE gcov)
-    find_program(LCOV_EXECUTABLE lcov)
-
-    if(NOT GCOV_EXECUTABLE)
-        message(FATAL_ERROR "Gcov not found.")
-    elseif(NOT LCOV_EXECUTABLE)
-        message(FATAL_ERROR "Lcov not found.")
-    endif()
-
-    list(APPEND COVERAGE_FLAGS 
-        "--coverage"
-        "-fprofile-arcs"
-        "-ftest-coverage"
-        )
-
-    add_custom_target(coverage
-        COMMAND ${LCOV_EXECUTABLE} --gcov-tool ${GCOV_EXECUTABLE}
-            -c -d . -o "${PROJECT_NAME}.info"
-
-        COMMAND ${LCOV_EXECUTABLE} --gcov-tool ${GCOV_EXECUTABLE}
-            --remove "${PROJECT_NAME}.info" ${LCOV_REMOVE_PATTERNS} 
-            -o "${PROJECT_NAME}.info"
-
-        COMMAND ${LCOV_EXECUTABLE} --gcov-tool ${GCOV_EXECUTABLE}
-            -l ${PROJECT_NAME}.info
-
-        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-    )
-
-else()
-    message(FATAL_ERROR "Coverage needs GCC or Clang")
-endif()
-
 function (add_coverage TNAME)
 	# only add coverage for target, if coverage is support and enabled.
     foreach (TNAME ${ARGV})
@@ -57,3 +19,44 @@ function(add_coverage_target TNAME)
             gcov
     )
 endfunction(add_coverage_target)
+
+if(ENABLE_COVERAGE)
+    # Coverage flags
+    if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
+        message(FATAL_ERROR "Not Implemented! Yet. Use GCC for coverage")
+    elseif(CMAKE_COMPILER_IS_GNUCXX)
+
+        find_program(GCOV_EXECUTABLE gcov)
+        find_program(LCOV_EXECUTABLE lcov)
+
+        if(NOT GCOV_EXECUTABLE)
+            message(FATAL_ERROR "Gcov not found.")
+        elseif(NOT LCOV_EXECUTABLE)
+            message(FATAL_ERROR "Lcov not found.")
+        endif()
+
+        list(APPEND COVERAGE_FLAGS 
+            "--coverage"
+            "-fprofile-arcs"
+            "-ftest-coverage"
+            )
+
+        add_custom_target(coverage
+            COMMAND ${LCOV_EXECUTABLE} --gcov-tool ${GCOV_EXECUTABLE}
+                -c -d . -o "${PROJECT_NAME}.info"
+
+            COMMAND ${LCOV_EXECUTABLE} --gcov-tool ${GCOV_EXECUTABLE}
+                --remove "${PROJECT_NAME}.info" ${LCOV_REMOVE_PATTERNS} 
+                -o "${PROJECT_NAME}.info"
+
+            COMMAND ${LCOV_EXECUTABLE} --gcov-tool ${GCOV_EXECUTABLE}
+                -l ${PROJECT_NAME}.info
+
+            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+        )
+
+    else()
+        message(FATAL_ERROR "Coverage needs GCC or Clang")
+    endif()
+endif(ENABLE_COVERAGE)
+
