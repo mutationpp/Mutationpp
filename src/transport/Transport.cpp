32ba2f6c8f7a5bcdde361908a72a91d187cd3e42
Matthew Goodson <matthew.goodson@corvidtec.com>
2023-11-16 21:25:12 -0500

    Use meanFreePath in coulombMeanCollisionTime.
    Use the `meanFreePath` function in the computation of
    `coulombMeanCollisionTime` to avoid redundant code.
    
    See #239.

--- src/transport/Transport.cpp
+++ src/transport/Transport.cpp
@@ -850,25 +850,7 @@ double Transport::coulombMeanCollisionTime()
 {
     if (!m_thermo.hasElectrons())
         return 0.0;
-
-    const int ns = m_thermo.nGas();
-    const double Th = m_thermo.T();
-    const double Te = m_thermo.Te();
-    const double nd = m_thermo.numberDensity();
-    const double* const X = m_thermo.X();
-    double sum = 0.0;
-    const Eigen::ArrayXd& Q11 = m_collisions.Q11ij();
-    const double Q11ee = m_collisions.Q11ee();
-    const Eigen::ArrayXd& Q11ei = m_collisions.Q11ei();
-    sum +=X[0]*X[0]*Q11ee;
-    for (int i = 1; i < ns; ++i)
-        sum +=X[i]*X[0]*Q11ei(i);
-
-    for (int i = 1; i < ns; ++i)
-        for (int j = 1; j < ns; ++j)
-            sum += X[i]*X[j]*Q11(i,j);
-
-    return (3.0/16.0)*1.0/(nd*sum)/averageHeavyThermalSpeed();//electronThermalSpeed();
+    return (3.0/16.0)*meanFreePath()/averageHeavyThermalSpeed();//electronThermalSpeed();
 }
 
 //==============================================================================
